861a373c65b2a0a35237ada492907b926beafc24
lippekstro <fellipecastro13@hotmail.com>
2023-11-08 13:08:21 -0300

    deletando duplicatas

--- index.php
+++ index.php
@@ -2,12 +2,34 @@
 require_once $_SERVER['DOCUMENT_ROOT'] . '/vitrine/layouts/_cabecalho.php';
 require_once $_SERVER['DOCUMENT_ROOT'] . '/vitrine/models/categoria.php';
 
+if (isset($_COOKIE['msg'])) {
+    setcookie('msg', '', time() - 3600, '/aulas/');
+    setcookie('tipo', '', time() - 3600, '/aulas/');
+}
+
 try {
     $categorias = Categoria::listar();
 } catch (PDOException $e) {
     echo $e->getMessage();
 }
+
 ?>
+
+<?php if (isset($_COOKIE['msg'])) : ?>
+    <?php if ($_COOKIE['tipo'] === 'sucesso') : ?>
+        <div class="alert alert-success text-center m-3" role="alert">
+            <?= $_COOKIE['msg'] ?>
+        </div>
+    <?php elseif ($_COOKIE['tipo'] === 'perigo') : ?>
+        <div class="alert alert-danger text-center m-3" role="alert">
+            <?= $_COOKIE['msg'] ?>
+        </div>
+    <?php else : ?>
+        <div class="alert alert-info text-center m-3" role="alert">
+            <?= $_COOKIE['msg'] ?>
+        </div>
+    <?php endif; ?>
+<?php endif; ?>
 
 <section class="jumbo">
     <img src="/vitrine/img/produtos.jpg" alt="" width="100%">
@@ -24,13 +46,13 @@ try {
 </section>
 
 <section class="grade">
-    <?php foreach($categorias as $c): ?>
-    <a href="/vitrine/views/produtos.php?categoria=<?= $c['nome_categoria'] ?>">
-        <div>
-            <h2 class="titulo-cat"><?= $c['nome_categoria'] ?></h2>
-            <img src="/vitrine/img/dummy.png" alt="" width="100%">
-        </div>
-    </a>
+    <?php foreach ($categorias as $c) : ?>
+        <a href="/vitrine/views/produtos.php?categoria=<?= $c['nome_categoria'] ?>">
+            <div>
+                <h2 class="titulo-cat"><?= $c['nome_categoria'] ?></h2>
+                <img src="/vitrine/img/dummy.png" alt="" width="100%">
+            </div>
+        </a>
     <?php endforeach; ?>
 </section>
 
