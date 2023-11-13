cee3c0180d91828b87ab51cbae0d1443e6b2f1fd
lippekstro <fellipecastro13@hotmail.com>
2023-11-13 10:39:37 -0300

    filtro

--- views/produtos.php
+++ views/produtos.php
@@ -10,19 +10,37 @@ try {
     } else {
         $termo = $_GET['categoria'];
         $produtos = Produto::listarPorCategoria($termo);
+        $categorias = Categoria::listar();
     }
 } catch (PDOException $e) {
     echo $e->getMessage();
 }
+
 ?>
 
-<section class="grade grade-produtos">
-    <?php foreach ($produtos as $p) : ?>
-        <div>
-            <h2 class="titulo-cat"><?= $p['nome_produto'] ?></h2>
-            <img src="data:image;charset=utf8;base64,<?= base64_encode($p['img_produto']); ?>" alt="" width="100%">
-        </div>
-    <?php endforeach; ?>
+<section class="filtros-produtos">
+    <section id="filtros">
+        <form action="<?= $_SERVER['PHP_SELF'] ?>" method="get">
+            <?php foreach ($categorias as $c) : ?>
+                <div class="radios">
+                    <input type="radio" name="categoria" id="categoria" value="<?= $c['nome_categoria'] ?>" <?= ($termo == $c['nome_categoria'] ? 'checked' : '') ?>>
+                    <label for="categoria"><?= $c['nome_categoria'] ?></label>
+                </div>
+            <?php endforeach; ?>
+            <div class="campo-form">
+                <input type="submit" value="Filtrar" class="btn self-center">
+            </div>
+
+        </form>
+    </section>
+    <section class="grade grade-produtos" id="produtos">
+        <?php foreach ($produtos as $p) : ?>
+            <div>
+                <h2 class="titulo-cat"><?= $p['nome_produto'] ?></h2>
+                <img src="data:image;charset=utf8;base64,<?= base64_encode($p['img_produto']); ?>" alt="" width="100%">
+            </div>
+        <?php endforeach; ?>
+    </section>
 </section>
 
 <?php
